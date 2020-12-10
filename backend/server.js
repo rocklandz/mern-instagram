5068ac580d384a854b3489c46e3ae69f09f4ba01
rocklandz <46998071+rocklandz@users.noreply.github.com>
2020-12-10 13:08:16 +0700

    ok

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import connectDb from './config/db.js';
 import dotenv from 'dotenv';
@@ -18,6 +19,20 @@ app.use(cors());
 //  Routes
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('Server is running');
+  });
+}
 
 //  Middlewares
 app.use(notFound);
