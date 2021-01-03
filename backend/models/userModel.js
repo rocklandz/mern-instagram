2041e458cdd87c4c48ed34da9b857c00863867c9
rocklandz <46998071+rocklandz@users.noreply.github.com>
2021-01-03 15:39:24 +0700

    update

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -9,7 +9,7 @@ const userSchema = mongoose.Schema(
     avatar: {
       type: String,
       default:
-        'https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_ohc=KhY8BuG5Py4AX-pjXPn&oh=7cc9d6508779d33ee5371a603e9b093a&oe=5FF1288F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2',
+        '//res.cloudinary.com/duczq6lyl/image/upload/v1609663119/rockland/e8x71f71l9a0mw6zxtqd.jpg',
     },
   },
   { timestamps: true }
